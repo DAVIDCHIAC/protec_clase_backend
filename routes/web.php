bb42491de2e07fbd25498038d0215d8fbc0511db
DAVIDCHIAC <achia861@unab.edu.co>
2025-09-11 10:59:13 -0500

    rutas basica

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get ("products",function(){
+    return"LISTADO DE PRODUCTOS";
+});
+Route::get ("products/create",function(){
+    return"FORMULARIO DE CADA PRODUCTO";
+});
+
+Route::get ("products/{name}/{categori}",function($name,$categori){
+    return"DETALLE DE CADA PRODUCTO" . $name."de ñ¿la categoria:" . $categori;
+});
+
+
+
+
