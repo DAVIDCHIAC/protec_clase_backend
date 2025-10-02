fc53ed7eab8b5dceeb586eafe271897bbec0efd7
DAVIDCHIAC <achia861@unab.edu.co>
2025-10-02 11:17:25 -0500

    modelo brand y categorys

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,17 @@
 <?php
 
-use App\Http\Controllers\ProductControler;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::profix("products")->Controller(ProductControler::class)->group (function(){
-Route::get ("products", "index");
-Route::get ("products/create","create");
-Route::get ("products/{name}/{categori}", "show");
-});
 
+Route::prefix("products")->controller(ProductController::class)->group(function () {
+    Route::get("/", "index");
+    Route::get("/create", "create");
+    Route::get("/{name}/{categori}", "show");
+});
 
 
 
